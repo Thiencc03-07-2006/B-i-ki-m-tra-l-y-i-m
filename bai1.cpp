7a39a5ac705f4357f5695e9cfc981daa37f1727d
Thiencc03-07-2006 <cct20062250.@gmail.com>
2024-11-21 19:01:52 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <cctype>
+using namespace std;
+
+string chuanHoaTen(string ten) {
+    int n = ten.size();
+    while (n > 0 && isspace(ten[0])) {
+        ten.erase(0, 1);
+        n--;
+    }
+    while (n > 0 && isspace(ten[n - 1])) {
+        ten.erase(n - 1, 1);
+        n--;
+    }
+    stringstream ss(ten);
+    string word;
+    vector<string> words;
+    
+    while (ss >> word) {
+        word[0] = toupper(word[0]);
+        for (int i = 1; i < word.size(); i++) {
+            word[i] = tolower(word[i]);
+        }
+        words.push_back(word);
+    }
+    string result = "";
+    for (int i = 0; i < words.size(); i++) {
+        result += words[i];
+        if (i != words.size() - 1) {
+            result += " ";
+        }
+    }
+    
+    return result;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    for (int i = 0; i < t; i++) {
+        string ten;
+        getline(cin, ten); // Ð?c h? tên t? dòng ti?p theo
+        cout << chuanHoaTen(ten) << endl; // In k?t qu? chu?n hóa
+    }
+    
+    return 0;
+}
